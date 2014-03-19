8ad10414d180f145cea0a3bfe4287df5f323b2a5
kryvosheyev <7612997@gmail.com>
2014-03-19 16:55:55 +0200

    kgs1commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kgs::Application.config.secret_key_base = 'ba9375a8043f9988245ca05b7170fa6c4a46c1352bd752292a4369108aa874eed4d0494d0acc9fd9aeb89706d1419ee1157769a9be9cb11a156dbae6772fb1ad'
