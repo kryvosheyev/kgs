7dc1bbda30989bc4dff153291a4bddc998e0ed3c
kryvosheyev <7612997@gmail.com>
2014-03-24 20:46:27 +0200

    added more devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Kgs::Application.routes.draw do
-  devise_for :users
   get "welcome/index"
   get "devise/registrations"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'home#index'
 
+  devise_for :users,:controllers => { :registrations =>'registration'}
+  get 'dashboard' => 'home#dashboard'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
