4c15489e607241d696582b2fbfe18ffe354d98bc
kryvosheyev <7612997@gmail.com>
2014-04-10 16:03:59 +0300

    added config.serve_static_assets = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,9 @@ Kgs::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Disable Rails's static asset server (Apache or nginx will already do this), 
+  # but Heroku doesn't.
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
